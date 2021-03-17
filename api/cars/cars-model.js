a9a4751c40e6e2b553c6e99ba9faea122c890e83
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-17 11:22:38 -0400

    imported the db-config and completed getAll and create

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,23 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+  return db('cars');
+};
 
 const getById = () => {
   // DO YOUR MAGIC
-}
+};
 
-const create = () => {
-  // DO YOUR MAGIC
-}
+const create = (cars) => {
+  return db('cars')
+    .insert(cars)
+    .then(([id]) => {
+      return db('cars').where('id', id).first();
+    });
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
